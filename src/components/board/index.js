d0f0a2eaa8e18d280c3025f3bcb3ba2bd7467702
athallahannafis <athallahannafis@gmail.com>
2019-11-10 16:35:20 +0700

    Add fire to board

--- src/components/board/index.js
+++ src/components/board/index.js
@@ -1,30 +1,40 @@
 import React from "react";
 
+import FireBox from "./fire";
 import Box from "./boardBox";
 import "./index.css"
 
 const boardRule = [
-    [1,0,1,0,1],
-    [0,1,1,1,0],
-    [0,0,1,0,0],
+    [3,0,3,0,3],
+    [0,3,3,3,0],
+    [0,0,3,0,0],
+    [3,3,3,3,3],
     [1,1,1,1,1],
     [1,1,1,1,1],
     [1,1,1,1,1],
-    [1,1,1,1,1],
-    [1,1,1,1,1],
-    [0,0,1,0,0],
-    [0,1,1,1,0],
-    [1,0,1,0,1]
+    [2,2,2,2,2],
+    [0,0,2,0,0],
+    [0,2,2,2,0],
+    [2,0,2,0,2]
 ];
 
 const boardData = boardRule.map((number) =>
     <div className="board">
         {
             number.map(function(type) {
-                if (type === 1) {
+                // if (type === 1) {
+                //     return <Box contain="white"/>
+                // } else {
+                //     return <Box contain="black"/>
+                // }
+                if (type === 2 ) {
+                    return <FireBox />
+                } else if (type === 1) {
+                    return <Box contain = "white" />
+                } else if (type === 3) {
                     return <Box contain="white"/>
                 } else {
-                    return <Box contain="black"/>
+                    return <Box contain="black" />
                 }
             }
         )}
