9454d7ec5f6d12ecfcf9c87ac0a9076fcd9a61d5
Azhar Rais <dev@azharaiz.com>
2019-12-15 18:52:46 +0700

    Improve homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import  { Link } from "react-router-dom";
+import React from "react";
+import { Link } from "react-router-dom";
+import "./index.css";
 
 class Index extends React.Component {
-    render() {
-        return(
-            <div className="App">
-                <h1>Dam Damn</h1>
-                <h2>Lets Play Dam Daman</h2>
-                <button><Link to="/game">Start Game!</Link></button>
-                <button><Link to="/about">About</Link></button>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="app">
+        <div className="container">
+          <h1 className="nama1">Tam Taman</h1>
+          <h2 className="nama2">A war between Fire and Tree</h2>
+          <div className="buttons">
+            <Link to="/setgame">
+              <button className="start-button">Start Game</button>
+            </Link>
+
+            <Link to="/about">
+              <button className="about-button">About</button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Index;
